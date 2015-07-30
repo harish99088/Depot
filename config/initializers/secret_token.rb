cc24dcca7c59a58bc81114cde665d33a39919269
harish99088 <rapuruharish123@gmail.com>
2015-07-30 23:37:34 +0530

    firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '1ced37786a6e487ee4f8b4d22feb350c2823333036e46d24a3990fbaeb67efa623cff9518b0b31f1ebd6da2ff7b7528c9ca0049fdf4abc5ab04309d042812093'
---
+++ db/migrate/20150730053945_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.decimal :price   
+    end
+  end
+end
